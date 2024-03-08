e6af0914657022c8546787e6e503544bdf42ed52
SHAHMEER AFTAB <154751468+aiwithshahmeer@users.noreply.github.com>
2024-03-09 01:51:52 +0500

    Add files via upload

---
+++ Greeting_names.js
@@ -0,0 +1,7 @@
+"use strict";
+let names = ['Mr Shahmeer', 'Mr Shahzil', 'Mr Mubashir', 'Mr Meraj', 'Mr Hammad'];
+const message = 'Welcome to Python class:  ';
+// for loop:
+for (let i = 0; i < names.length; i++) {
+    console.log(message + names[i]);
+}
---
+++ Greeting_names.ts
@@ -0,0 +1,13 @@
+
+
+let names :string[] =['Mr Shahmeer', 'Mr Shahzil', 'Mr Mubashir', 'Mr Meraj', 'Mr Hammad'] ;
+
+const message:string='Welcome to Python class:  ';
+
+
+// for loop:
+for(let i=0; i<names.length; i++){
+
+    console.log(message + names[i]) ;
+
+}
